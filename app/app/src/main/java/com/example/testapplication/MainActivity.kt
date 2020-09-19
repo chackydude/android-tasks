3842acf1acf78309b78dd258ea29951a72b2e858
Daniil Trofimov <trofimovdanil946@gmail.com>
2020-09-19 14:56:42 +0300

    some layout fixes, added changable field

--- app/app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.testapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editButton.setOnClickListener {
+            if (field.visibility == View.VISIBLE) {
+                field.visibility = View.INVISIBLE
+                editField.visibility = View.VISIBLE
+                editButton.setText("Save information");
+            } else {
+                var editFieldText: String = editField.getText().toString()
+                if (editFieldText == "") {
+                    editFieldText = "Some field text"
+                }
+                field.setText(editFieldText)
+                field.visibility = View.VISIBLE
+                editField.visibility = View.GONE
+                editButton.setText("Edit information");
+            }
+        }
     }
 }
